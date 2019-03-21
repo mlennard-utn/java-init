c4734fdaa4bf5598297d661142c6d98c5302692d
Martin Lennard <mlennard.utn@gmail.com>
2019-03-20 21:42:26 -0300

    Subo la clase con el If

---
+++ java-git-ultima-chance/src/ejemplo/CondicionIf.java
@@ -0,0 +1,13 @@
+package ejemplo;
+
+public class CondicionIf {
+
+	public static void main(String[] args) {
+		int valor = 1;
+		if(valor < 0) {
+			System.out.println("es negativo");
+		}
+
+	}
+
+}
