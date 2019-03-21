47453bc2b306b2a58147c10e04978247321d58be
alex de assis <alexdeassis@gmail.com>
2019-03-20 22:05:19 -0300

    Modificacion del if

--- java-git-ultima-chance/src/ejemplo/CondicionIf.java
+++ java-git-ultima-chance/src/ejemplo/CondicionIf.java
@@ -3,11 +3,15 @@ package ejemplo;
 public class CondicionIf {
 
 	public static void main(String[] args) {
-		int valor = 1;
+		int valor = 5;
 		if(valor < 0) {
 			System.out.println("es negativo");
+		} else if (valor > 0) {
+			System.out.println("es positivo");
+		} else {
+			System.out.println("es un cero");
 		}
-
+		System.out.println("El valor es: " + valor + " <- ese fue valor");
 	}
 
 }
